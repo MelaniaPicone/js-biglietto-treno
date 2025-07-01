43ddf2e10251eef343fb00089f06a5e4493504c9
MelaniaPicone <melania_picone@yahoo.it>
2025-07-01 18:31:35 +0200

    [add] initizal prompt

---
+++ javascript/script.js
@@ -0,0 +1,7 @@
+// richiesta all'utente
+const distanceNum = prompt('Inserisci qui la distanza che vuoi percorrere');
+const yearsNum = prompt('Inserisci qui la tua età');
+console.log(distanceNum);
+console.log(yearsNum);
+
+
