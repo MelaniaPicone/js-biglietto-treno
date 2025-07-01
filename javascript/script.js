4ffe3646ae94d2121f0e9be901902be298cf81eb
MelaniaPicone <melania_picone@yahoo.it>
2025-07-01 18:35:20 +0200

    [add] discount

--- javascript/script.js
+++ javascript/script.js
@@ -9,3 +9,21 @@ const ticket = 0.21;
 const ticketTot = ticket * distanceNum;
 console.log(ticketTot);
 
+// applicazione dello sconto
+const minNum = 18;
+const overNum = 65;
+
+const ticketDiscountMin = 0.20;
+const ticketDiscountOver = 0.40;
+
+if(yearsNum < minNum) {
+console.log("viene applicato uno sconto del 20% sul biglietto")
+}
+
+else if(yearsNum >= overNum){
+  console.log ("viene applicato uno sconto del 40% sul biglietto")
+}
+
+else {
+console.log("il biglietto non è soggetto ad alcuno sconto")
+}
